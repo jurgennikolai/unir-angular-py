a2c597bcf62f9d59738d1f90c85a9a6fcebadfc6
jurgennikolai <jurgennikolai@gmail.com>
2025-01-27 14:15:34 -0500

    v1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'enviroments/enviroment';
+import { environment } from 'src/enviroments/enviroment.prod';
 
 @Component({
   selector: 'app-root',
