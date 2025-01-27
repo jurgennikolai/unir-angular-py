b7f52a045bc9138c54144eebc01fbcdcdbabdcd9
jurgennikolai <jurgennikolai@gmail.com>
2025-01-27 13:28:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'enviroments/enviroment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular_py';
+  env = environment;
+  mssg !: string;
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit(): void {
+    console.log("GHA", environment.apiUrl)
+  }
+
+  getStatus(){
+    this._http.get<any>(this.env.apiUrl).subscribe({
+      next: (res)=>{
+        this.mssg = res
+      },
+      error: (e) =>
+        (this.mssg = 'Aún no se puede conectar a la BD, reintente de nuevo.'),
+    });
+
+  }
 }
